4fceaec384df46be30949bd45c5abca818ffa63a
Friend.Wu <407339176@qq.com>
2019-09-27 15:55:46 +0800

    project init & third lib import

---
+++ TenonVPNMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TenonVPNMac
+//
+//  Created by friend on 2019/9/27.
+//  Copyright © 2019 friend. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
